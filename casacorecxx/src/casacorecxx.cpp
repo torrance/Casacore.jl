7e719a4acf20e6927298b26483ff50b973fecb3b
Torrance Hodgson <torrance123@gmail.com>
2022-09-30 07:57:50 +0000

    Obviously, I'm not a C++ programmer

--- casacorecxx/src/casacorecxx.cpp
+++ casacorecxx/src/casacorecxx.cpp
@@ -9,33 +9,8 @@ using namespace casacore;
 
 // Define super types to allow upcasting, which in turn allows class hierarchies
 namespace jlcxx {
-    template<> struct SuperType<ScalarColumnDesc<Bool>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<Char>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<uChar>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<Short>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<uShort>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<Int>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<uInt>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<Int64>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<Float>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<Double>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<Complex>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<DComplex>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ScalarColumnDesc<String>> { typedef BaseColumnDesc type; };
-
-    template<> struct SuperType<ArrayColumnDesc<Bool>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<Char>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<uChar>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<Short>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<uShort>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<Int>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<uInt>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<Int64>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<Float>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<Double>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<Complex>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<DComplex>> { typedef BaseColumnDesc type; };
-    template<> struct SuperType<ArrayColumnDesc<String>> { typedef BaseColumnDesc type; };
+    template<typename T> struct SuperType<ScalarColumnDesc<T>> { typedef BaseColumnDesc type; };
+    template<typename T> struct SuperType<ArrayColumnDesc<T>> { typedef BaseColumnDesc type; };
 }
 
 JLCXX_MODULE define_julia_module(jlcxx::Module &mod) {
