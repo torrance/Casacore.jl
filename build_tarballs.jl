46969c479e35cd9c5b3af00be1e756b488467f7d
Torrance Hodgson <torrance123@gmail.com>
2022-10-20 03:45:50 +0000

    Reorganise file structure

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -38,5 +38,5 @@ dependencies = [Dependency("libcxxwrap_julia_jll"),
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.7",
-               preferred_gcc_version=v"7") # We need C++17 for CxxWrap
+               julia_compat="1.8",
+               preferred_gcc_version=v"12") # We need C++17 for CxxWrap
